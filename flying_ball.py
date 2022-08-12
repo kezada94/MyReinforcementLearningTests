0f59fd4ab38e7f62357bc109690f4cb0e89b96c3
Felipe Quezada <kezada66@gmail.com>
2022-08-12 17:05:55 -0400

    improved several stuff towards gym.Env

--- flying_ball.py
+++ flying_ball.py
@@ -1,10 +1,14 @@
-from SimpleRenderer import SimpleRenderer
+from simple_renderer import SimpleRenderer
 import random
 from math import *
 from PIL import Image
 import numpy as np
+import enum
+CIRCLE_VERTICES = 6
 
-CIRCLE_VERTICES = 32
+class Actions(enum.Enum):
+    Noot = 0
+    Jump = 1
 
 class Ball():
     def __init__(self, radius, color):
@@ -12,6 +16,7 @@ class Ball():
         self.position = np.array([0,0]);
         self.color = color
         self.velocity = np.array([0,0]);
+        self.isAlive = False
         self.genModel()
 
     def genModel(self):
@@ -52,34 +57,47 @@ class Camera():
         return newVertices
 
 class FlyingBallGame():
-    def __init__(self, viewportWidth, viewportHeight, cameraHeight, gravity=np.array([0, -0.1]), playerRadius=5, playerJumpVelocity=np.array([0.0, 3]), enemyRadius=10, enemyVelocity=np.array([-1, 0.0]), maxEnemies=10, enemyProb=0.0005, gameSpeed=1):
+    def __init__(self, viewportWidth, viewportHeight, cameraHeight, gravity=np.array([0, -0.1]), playerRadius=5, playerJumpVelocity=np.array([0.0, 3]), enemyRadius=10, enemyVelocity=np.array([-1, 0.0]), maxEnemies=10, enemyProb=0.0005, gameSpeed=1, seed=0):
         self.r = SimpleRenderer(viewportWidth, viewportHeight)
+        self.r.init()
+        random.seed(seed)
+    
         self.aspectRatio = viewportWidth/viewportHeight
         self.camera = Camera(np.array([0, 0]), self.aspectRatio, cameraHeight)
-        self.r.init()
         self.gameShouldQuit = False
+        self.gameShouldReset = False
         self.lastTime = 0
         self.viewportWidth, self.viewportHeight = viewportWidth, viewportHeight
         self.gameSpeed = gameSpeed
-
+        self.score = 0
+        self.framesSinceReset = 0
         self.player = Ball(playerRadius, [255, 0, 0])
+        self.player.isAlive = True
         self.playerJumpVelocity = playerJumpVelocity
         self.enemyVelocity = enemyVelocity
         self.enemyRadius = enemyRadius
-        self.enemies = []
         self.maxEnemies = maxEnemies
+        self.enemies = [Ball(enemyRadius, [0, 255, 0]) for x in range(self.maxEnemies)]
+        self.enemiesAlive = 0
         self.enemyProb = enemyProb
         self.gravity = gravity
-
         self.spawnRegion = Region(np.array([100,100]), np.array([100,-100]))
         self.despawnRegion = Region(np.array([-102,100]), np.array([-100,-100]))
         self.aliveRegion = Region(np.array([-100,100]), np.array([100,-100]))
         self.reset()
 
     def reset(self):
-        self.player.position = np.array([0.0,0.0])
-        self.player.velocity = np.array([0.0,0.0])
-        self.enemies = []
+        self.player.position = np.array([0.0, 0.0])
+        self.player.velocity = np.array([0.0, 0.0])
+        self.player.isAlive = True
+
+        #could be only for alive enemies
+        for enemy in self.enemies:
+            enemy.isAlive = False
+        self.score = 0
+        self.framesSinceReset = 0
+        self.gameShouldReset=False
+        self.enemiesAlive = 0
 
     def updatePhysics(self):
         self.player.velocity += self.gravity*self.gameSpeed
@@ -97,59 +115,89 @@ class FlyingBallGame():
         #    self.player.position[1] = self.player.radius
 
     def processInput(self, input):
+        actions = []
         if input[0]:
             self.gameShouldQuit = True
         if input[1]:
-            self.playerJump()
+            actions.append(Actions.Jump)
+            #self.playerJump()
         if input[3]:
             self.camera.height += 1
         if input[4]:
             # this cant be zero
             self.camera.height -= 1
-
+            assert self.camera.height>0
+        if input[5]:
+            self.gameShouldReset = True
+        return actions
 
     def drawScene(self):
         worldVertices = self.player.toWorldSpace()
         viewVertices = self.camera.toViewSpace(worldVertices)
         self.r.drawShape(viewVertices, self.player.color)
         
-        b = Ball(5, np.array([0,0,255]))
-        b.position = np.array([0,50])
-        wb = b.toWorldSpace()
-        vb = self.camera.toViewSpace(wb)
-        self.r.drawShape(vb, b.color)
+        #b = Ball(5, np.array([0,0,255]))
+        #b.position = np.array([0,50])
+        #wb = b.toWorldSpace()
+        #vb = self.camera.toViewSpace(wb)
+        #self.r.drawShape(vb, b.color)
 
         for enemy in self.enemies:
-            worldVerticesE = enemy.toWorldSpace()
-            viewVerticesE = self.camera.toViewSpace(worldVerticesE)
-            self.r.drawShape(viewVerticesE, enemy.color)
+            if enemy.isAlive:
+                worldVerticesE = enemy.toWorldSpace()
+                viewVerticesE = self.camera.toViewSpace(worldVerticesE)
+                self.r.drawShape(viewVerticesE, enemy.color)
 
 
     def spawnEnemy(self):
-        if len(self.enemies) < self.maxEnemies and random.random() < self.enemyProb:
-            enemy = Ball(self.enemyRadius, [0, 255, 0])
+        if self.enemiesAlive < self.maxEnemies and random.random() < self.enemyProb:
+            #print("1", [e.isAlive for e in self.enemies])
+
+            print("Spawning enemy.")
+            enemy = None
+            for e in self.enemies:
+                if not e.isAlive:
+                    enemy = e
+                    break
+            assert enemy is not None
+            #print("2", [e.isAlive for e in self.enemies])
+
             x = random.uniform(self.spawnRegion.topLeft[0], self.spawnRegion.bottomRight[0])
             y = random.uniform(self.spawnRegion.bottomRight[1], self.spawnRegion.topLeft[1])
             enemy.position = np.array([x, y])
             enemy.velocity = self.enemyVelocity
-            self.enemies.append(enemy)
+            enemy.isAlive = True
+            self.enemiesAlive+=1
+
+            #self.enemies.append(enemy)
 
     def distance(self, pos1, pos2):
         return sqrt(abs(pos1[0]-pos2[0])**2 + abs(pos1[1]-pos2[1])**2)
 
     def checkCollision(self):
-        delete = []
+        #delete = []
         for enemy in self.enemies:
+            if not enemy.isAlive:
+                continue
             if self.despawnRegion.isIn(enemy.position):
-                delete.append(enemy)
+                self.despawnEnemy(enemy)
             if (self.distance(self.player.position, enemy.position) <= max(self.player.radius, enemy.radius)):
-                self.gameShouldQuit = True
+                #print("player out of collision")
+                self.player.isAlive = False
+                #self.gameShouldReset = True
         if not self.aliveRegion.isIn(self.player.position):
-            self.gameShouldQuit = True
+            #print("player out of region")
+            self.player.isAlive = False
+            #self.gameShouldReset = True
 
-        for d in delete[::-1]:
-            self.enemies.remove(d)
+        #for d in delete[::-1]:
+        #    self.enemies.remove(d)
+    def despawnEnemy(self, enemy):
+        enemy.isAlive = False
+        self.enemiesAlive-=1
+        assert self.enemiesAlive>-1
 
+    ## updateFreq is the minimum ms between frames
     def mainLoop(self, updateFreq=16.00):
         self.updateFreq = updateFreq
         self.lastTime = self.r.time()
@@ -158,24 +206,59 @@ class FlyingBallGame():
             self.deltaTime = now - self.lastTime
             if (self.deltaTime < updateFreq):
                 continue
-            print(1/self.deltaTime*1000)
+            #print(1/self.deltaTime*1000)
             self.lastTime = now
-            self.r.clear()
             input = self.r.captureInput()
-            self.processInput(input)
-            self.spawnEnemy()
-            self.updatePhysics()
-            self.checkCollision()
-            self.drawScene()
-            self.r.flipBuffer()
+            actions = self.processInput(input)
+            self.step(actions)
+            if self.gameShouldReset:
+                self.reset()
 
+    def close(self):
+        self.r.close()
+
+    def impartLogic(self):
+        if not self.player.isAlive:
+            self.gameShouldReset = True
+
+    def updateScore(self):
+        if self.player.isAlive:
+            self.framesSinceReset += 1
+            if self.framesSinceReset == 60:
+                self.framesSinceReset = 0
+                self.score += 1
+                print(self.score)
+
+    def performActions(self, actionsVector):
+        for action in actionsVector:
+            if action == Actions.Jump:
+                self.playerJump()
+
+    def step(self, actionsVector):
+        if not self.player.isAlive:
+            return
+        self.r.clear()
+        self.spawnEnemy()
+        self.performActions(actionsVector)
+        self.updatePhysics()
+        self.checkCollision()
+        #self.impartLogic()
+        self.updateScore()
+        self.drawScene()
+        self.r.flipBuffer()
+        
+        #else:
+        #    self.reset()
+        #    self.gameShouldReset = False
     
         # capture input
 
 
-game = FlyingBallGame(800, 800, 200, playerRadius=5,
-                    enemyRadius=5, enemyProb=0.004, gameSpeed=1)
-game.mainLoop()
-frame = game.r.exportFrameAs3DArray()
-im = Image.fromarray(np.transpose(frame[::2, ::2], (1, 0, 2)), 'RGB')
-im.save("lastFrame.png")
+# game = FlyingBallGame(800, 600, 200, playerRadius=5,
+#                    enemyRadius=5, enemyProb=0.4, gameSpeed=1,
+#                    enemyVelocity=np.array([-1, 0.0]), seed=0)
+# game.mainLoop()
+# frame = game.r.exportFrameAs3DArray()
+# game.close()
+# im = Image.fromarray(np.transpose(frame[::2, ::2], (1, 0, 2)), 'RGB')
+# im.save("lastFrame.png")
